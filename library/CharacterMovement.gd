3cb3335066a66f25f102913f406892c060fa58d8
Lex Guo <lexguo3001@gmail.com>
2023-03-31 17:45:50 -0400

    Basic Movement Improvements
    Added friction to movement and made character face the mouse.

--- library/CharacterMovement.gd
+++ library/CharacterMovement.gd
@@ -1,11 +1,25 @@
 extends CharacterBody2D
 
 @export var speed = 400
+var friction = 0.1
+var acceleration = 0.7
 
 func get_input():
-	var input_direction = Input.get_vector("left", "right", "up", "down")
-	velocity = input_direction.normalized() * speed
+	#Horizontal movement
+	var xDir = Input.get_axis("left", "right")
+	if xDir != 0:
+		velocity.x = lerp(velocity.x, xDir * speed, acceleration)
+	else:
+		velocity.x = lerp(velocity.x, 0.0, friction)
 
-func _physics_process(delta):
+	#Vertical movement
+	var yDir = Input.get_axis("up", "down")
+	if yDir != 0:
+		velocity.y = lerp(velocity.y, yDir * speed, acceleration)
+	else:
+		velocity.y = lerp(velocity.y, 0.0, friction)
+
+func _physics_process(_delta):
 	get_input()
 	move_and_slide()
+	look_at(get_global_mouse_position())
