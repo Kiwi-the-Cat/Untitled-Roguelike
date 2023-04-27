763655ff2e43ce9537acaf99e9cb4ea000554c2d
Lex Guo <lexguo3001@gmail.com>
2023-04-27 17:32:45 -0400

    Added roll function
    Bound to shift key
    Not the prettiest, but it gets the job done for now.

--- library/CharacterMovement.gd
+++ library/CharacterMovement.gd
@@ -1,10 +1,20 @@
 extends CharacterBody2D
 
-@export var speed = 400
+@export var speed = 200
 var friction = 0.1
 var acceleration = 0.7
 
+var rolling = false
+var roll_timer = 0
+var roll_cooldown = 0
+
 func get_input():
+	#Speed adjustment if rolling
+	if roll_timer > 30:
+		speed = 800
+	else:
+		speed = 200
+	
 	#Horizontal movement
 	var xDir = Input.get_axis("left", "right")
 	if xDir != 0:
@@ -19,7 +29,22 @@ func get_input():
 	else:
 		velocity.y = lerp(velocity.y, 0.0, friction)
 
-func _physics_process(_delta):
+func roll():
+	if Input.is_action_just_pressed("roll") && roll_timer <= 0 && roll_cooldown <= 0:
+		roll_timer = 50
+	if roll_timer > 0:
+		roll_timer -= 1
+		roll_cooldown = 100
+		rolling = true
+	elif roll_cooldown >= 0:
+		roll_cooldown -= 1
+		rolling = false
+
+func _physics_process(delta):
+	roll()
 	get_input()
 	move_and_slide()
-	look_at(get_global_mouse_position())
+	if rolling:
+		rotation += 20 * delta
+	else:
+		look_at(get_global_mouse_position())
