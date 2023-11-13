ae024455a226aa5113c2a7cd81bda014cce643da
Max <remiozzy@gmail.com>
2023-11-13 16:37:28 -0500

    Fixing merge errors is not fun

--- library/CharacterMovement.gd
+++ library/CharacterMovement.gd
@@ -1,5 +1,7 @@
 extends CharacterBody2D
 
+signal knife_active
+
 #Physics varables
 @export var speed:int = 300
 @export var friction:float = 0.1
@@ -73,9 +75,15 @@ func _process(delta):
 	var xPercent:float = mousePos.x / screenX
 	var yPercent:float = mousePos.y / screenY
 	
-	print(get_angle_to(get_global_mouse_position()))
 
-
+	#Enables knife attack when mouse is clicked
+	if Input.is_action_pressed("attack"):
+		$Knife.visible = true
+		$Knife/KnifeHitbox.disabled = false
+		$Knife.look_at(Vector2(mousePos.x, mousePos.y))
+	else:
+		$Knife.visible = false
+		$Knife/KnifeHitbox.disabled = true
 
 func _on_exit_body_entered(body): #Level Transistions
 	match get_tree().current_scene.name:
@@ -110,4 +118,7 @@ func update_animation_parameters():
 
 
 
+
+func _on_knife_body_entered(body):
+	knife_active.emit()
 
