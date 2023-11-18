13d07c85736b9a393a46cd2e365159a3351a67fb
Lex Guo <lexguo3001@gmail.com>
2023-11-18 10:26:39 -0500

    Enemy attack update
    Enemy runs away after hitting, need to make it stop and attack again

--- enemies/enemy.gd
+++ enemies/enemy.gd
@@ -5,24 +5,31 @@ extends CharacterBody2D
 #Combat variables
 @export var health:int = 20
 signal enemyAttack
+var firstHit:bool = true #Fixes enemy instantly hitting on scene start
+var attacking:bool = true
 
-#Movement
+#Movement Variables
 @onready var navAgent = $NavigationAgent2D
-var target:Vector2;
+var target:Vector2
 var speed:int = 100
+var currentPos:Vector2
+var nextPos:Vector2
 
 func _process(delta):
 	move()
 
 func move():
-	target = Vector2(player.position.x, player.position.y)
+	target = player.position
 	navAgent.set_target_position(target)
 	
-	if(position.distance_to(target) > 0.5):
-		var currentPos = global_transform.origin
-		var nextPos = navAgent.get_next_path_position()
-		velocity = (nextPos - currentPos).normalized() * speed
-		move_and_slide()
+	currentPos = global_transform.origin
+	nextPos = navAgent.get_next_path_position()
+	velocity = (nextPos - currentPos).normalized() * speed
+	
+	if(!attacking): #Prevents enemy from repeatedly hitting player
+		velocity *= -1
+	
+	move_and_slide()
 
 func hurt():
 	health -= 2
@@ -35,4 +42,8 @@ func _on_player_knife_active():
 	hurt()
 
 func _on_enemy_area_body_entered(body):
-	enemyAttack.emit()
+	if(!firstHit):
+		enemyAttack.emit()
+		attacking = false;
+	else:
+		firstHit = false;
