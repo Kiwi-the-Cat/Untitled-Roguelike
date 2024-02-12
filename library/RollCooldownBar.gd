b4ce63d756f3f4702c6284326d508181e39ae3b6
Max Gallo <remiozzy@gmail.com>
2024-02-12 16:45:49 -0500

    Deleted everything, starting anew with better organization

--- library/RollCooldownBar.gd
+++
@@ -1,8 +0,0 @@
-extends ProgressBar
-func _process(delta):
-	#Sets the cooldown bar to percentage of cooldown remaining divided by total cooldown time
-	if get_parent().roll_cooldown.get_time_left() > 0:
-		visible = true
-		value = get_parent().roll_cooldown.get_time_left() / (get_parent().roll_time * 5) * 100
-	else:
-		visible = false
