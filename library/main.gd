d4ec8cf763af1eb656d19a7a598c6eb7b602bd58
Max Gallo <remiozzy@gmail.com>
2024-03-07 16:46:27 -0500

    Level transitions

--- library/main.gd
+++ library/main.gd
@@ -3,7 +3,7 @@ extends Node2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	get_tree().change_scene_to_file("res://level/level_1.tscn")
+	pass
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
