1b0a5b137ad914ef7b93bb51e92826b0b213a006
Lex Guo <lexguo3001@gmail.com>
2024-04-09 18:31:55 -0400

    Multiple slime enemy updates
    - Faces Player
    - Separated movement and combat hurboxes for future convinience
    - Cleaned up death code
    - Cleaned up attack timers

--- enemies/enemy.gd
+++ enemies/enemy.gd
@@ -1,58 +1,63 @@
 extends CharacterBody2D
 
-@onready var player = get_parent().get_node("Player")
-@onready var sprite = $AnimatedSprite2D
+@onready var player:CharacterBody2D = get_parent().get_node("Player")
+@onready var sprite:AnimatedSprite2D = $AnimatedSprite2D
+@onready var spriteFrames:SpriteFrames = sprite.sprite_frames
 
 #Combat variables
-@export var health:int = 20
 @onready var attackTimer:Timer = $Hitbox/AttackTimer
-@onready var retreatTimer:Timer = $Hitbox/RetreatTimer
+var health:int = 20
 var attacking:bool = true
 var state:String = "idle"
 
 #Movement Variables
 @onready var navAgent = $NavigationAgent2D
-var target:Vector2
 @export var speed:float = 200
+var target:Vector2
 var currentPos:Vector2
 var nextPos:Vector2
 
 func _process(_delta):
-	sprite.play(state)
-	
 	$HealthBar.value = health
 	if(player != null && state != "death"):
 		move()
 	
 	#Checks for dying
-	if(sprite.animation == "death" && sprite.frame == 4):
-		self.free()
-	elif(health <= 0):
+	if(health <= 0):
+		sprite.play("death")
 		state = "death"
+	else:
+		sprite.play(state)
 
 func move():
-	target = Vector2(player.position.x, player.position.y)
+	target = player.position
 	navAgent.set_target_position(target)
 	
 	currentPos = global_transform.origin
 	nextPos = navAgent.get_next_path_position()
 	velocity = (nextPos - currentPos).normalized() * speed
 	
-	if(state == "attack"):
+	if(state == "attack"): #pause for attack animation to play
 		velocity *= 0
 	elif(!attacking): #Prevents enemy from repeatedly hitting player
 		velocity *= -1
 	
+	sprite.flip_h = target.x > self.position.x
 	move_and_slide()
 
-func _on_retreat_timer_timeout():
-	state = "idle"
-
 func _on_attack_timer_timeout():
-	attacking = true
+	match(state):
+		"attack":
+			state = "idle"
+			attackTimer.start(1)
+		"idle":
+			attacking = true
 
 func _on_hitbox_area_entered(_area):
-	retreatTimer.start(0.5)
-	attackTimer.start(1.5)
+	attackTimer.start(spriteFrames.get_frame_count("attack") / spriteFrames.get_animation_speed("attack"))
 	attacking = false
 	state = "attack"
+
+func _on_animated_sprite_2d_frame_changed():
+	if(state == "death" && sprite.frame == 5):
+		self.queue_free()
