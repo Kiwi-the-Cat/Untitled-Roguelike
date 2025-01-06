dd216d270109846660609bce8b5e44b803881ae2
kass <remiozzy@gmail.com>
2025-01-06 17:32:35 -0500

    worked on enemy 4hrs

--- enemies/enemy.gd
+++ enemies/enemy.gd
@@ -1,69 +1,18 @@
 extends CharacterBody2D
 
-@onready var player : CharacterBody2D = get_parent().get_node("Player")
-@onready var sprite : AnimatedSprite2D = $Sprite
-@onready var spriteFrames : SpriteFrames = sprite.sprite_frames
+@export var speed : int = 25 
+var playerChase : bool = false
+var player = null
 
-#Combat variables
-@onready var attackTimer : Timer = $Hitbox/AttackTimer
-var health : int = 20
-var attacking : bool = true
-var state : String = "idle"
-signal death
+func _physics_process(delta: float) -> void:
+	if playerChase:
+		position += (player.position - position)/speed
 
-#Movement Variables
-@onready var navAgent : NavigationAgent2D = $NavigationAgent2D
-@export var speed : float = 200
-var target : Vector2
-var currentPos : Vector2
-var nextPos : Vector2
-var detect_range : int = 200
+func _on_detection_area_body_entered(body: Node2D) -> void:
+	player = body
+	playerChase = true
 
-func _process(_delta):
-	$HealthBar.value = health
-	if(player != null && state != "death"):
-		move()
-	
-	#Checks for dying
-	if(health <= 0):
-		sprite.play("death")
-		state = "death"
-	else:
-		sprite.play(state)
 
-func move():
-	if(abs(player.position.x - position.x) < detect_range && abs(player.position.y - position.y) < detect_range):
-		target = player.position
-		navAgent.set_target_position(target)
-		
-		currentPos = global_transform.origin
-		nextPos = navAgent.get_next_path_position()
-		velocity = (nextPos - currentPos).normalized() * speed
-		
-		if(state == "attack"): #Pause for attack animation to play
-			velocity *= 0
-		elif(!attacking): #Prevents enemy from repeatedly hitting player
-			velocity *= -1
-		
-		sprite.flip_h = target.x > self.position.x
-		move_and_slide()
-
-func _on_attack_timer_timeout():
-	match(state):
-		"attack":
-			state = "idle"
-			attackTimer.start(1)
-		"idle":
-			attacking = true
-
-func _on_hitbox_area_entered(area): #Enemy hits player
-	if(area.get_parent().get_name() == "Player"): #Ensures that enemies don't collide with each other's hitboxes
-		attackTimer.start(spriteFrames.get_frame_count("attack") / spriteFrames.get_animation_speed("attack"))
-		attacking = false
-		state = "attack"
-		health -= 10 #Used for debugging, remove for actual game
-
-func _on_animated_sprite_2d_frame_changed():
-	if(state == "death" && sprite.frame == 5):
-		death.emit()
-		self.queue_free()
+func _on_detection_area_body_exited(body: Node2D) -> void:
+	player = null
+	playerChase = false
