20bc7ce85dc0931d2ad59a563d3be4d25686f1b5
Lex Guo <lexguo3001@gmail.com>
2025-04-08 19:37:27 -0400

    Frog boss animation update
    Accidentally used animation for small frogs initially

--- enemies/frog_boss.gd
+++ enemies/frog_boss.gd
@@ -2,3 +2,6 @@ extends CharacterBody2D
 #NOTES FOR LATER
 # Summon animation should play when player enters detection area
 # Detection area should be entire stage minus entrance
+
+func jump_attack():
+	pass
