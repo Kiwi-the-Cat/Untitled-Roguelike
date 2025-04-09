b91703dca036782018cc8f780c5995ed49de4485
kass <remiozzy@gmail.com>
2025-04-09 18:02:16 -0400

    combat system work, 2hrs

--- library/enemy.gd
+++ library/enemy.gd
@@ -1,38 +1,58 @@
 extends CharacterBody2D
 
-var playerChase : bool = false
+@export var health : int
+
+var player_chase : bool = false
 var player = null
+var player_in_range : bool
 
 #Movement Variables
 @onready var navAgent : NavigationAgent2D = $NavigationAgent2D
 @export var speed : float = 100
 var target : Vector2
-var currentPos : Vector2
-var nextPos : Vector2
+var current_pos : Vector2
+var next_pos : Vector2
 var detect_range : int = 200
 
 func _ready() -> void:
 	$Sprite.play("idle")
 
 func _physics_process(delta: float) -> void:
-	if(playerChase):
+	deal_damage()
+	
+	if(player_chase):
 		target = player.position
 		navAgent.set_target_position(target)
 		
-		currentPos = global_transform.origin
-		nextPos = navAgent.get_next_path_position()
-		velocity = (nextPos - currentPos).normalized() * speed
+		current_pos = global_transform.origin
+		next_pos = navAgent.get_next_path_position()
+		velocity = (next_pos - current_pos).normalized() * speed
 		
 		$Sprite.flip_h = target.x > self.position.x
 		move_and_slide()
 
 func _on_detection_area_body_entered(body: Node2D) -> void:
 	player = body
-	playerChase = true
+	player_chase = true
 
 func _on_detection_area_body_exited(body: Node2D) -> void:
 	player = null
-	playerChase = false
+	player_chase = false
 
 func enemy():
 	pass
+
+func _on_hit_box_body_entered(body: Node2D) -> void:
+	if body.has_method("player"):
+		player_in_range = true
+
+
+func _on_hit_box_body_exited(body: Node2D) -> void:
+	if body.has_method("player"):
+		player_in_range = false
+	
+func deal_damage() -> void:
+	if player_in_range and Global.player_current_attack:
+		health -= 1
+		if health <= 0:
+			self.queue_free()
---
+++ library/global.gd
@@ -0,0 +1,3 @@
+extends Node
+
+var player_current_attack := false
