cf4d9037dcdcff08a3519c5a621bea29f6faa110
Lex Guo <lexguo3001@gmail.com>
2025-04-09 18:40:29 -0400

    Basic Boss Fight Initiation
    Sets player camera and traps player in boss area

--- level/Area_1/area_1_boss.gd
+++ level/Area_1/area_1_boss.gd
@@ -1,8 +1,18 @@
 extends Node2D
 
 func _ready():
-	print(get_parent())
+	$Player.setCameraLimits(-90, 630, -40, 1240)
 	if (get_parent().scene_file_path == "res://level/main.tscn"):
-		$Player.setCameraLimits(-95, 510, -65, 1250)
 		get_parent().set_exits([$Exit1/CollisionShape2D, $Exit2/CollisionShape2D])
 		get_parent().set_enemy_count(2)
+
+#Starts the boss fight when player walks into area
+func _on_area_2d_body_entered(body: Node2D) -> void: 
+	#Enable invisible wall preventing player from exiting area
+	$Walls.enabled = true
+	
+	#Expands player camera so they can see the full field
+	$Player/Camera2D.zoom = Vector2(0.9, 0.9)
+	
+	#Disables self to prevent accidental resetting of the boss
+	$BossStart.monitoring = false
